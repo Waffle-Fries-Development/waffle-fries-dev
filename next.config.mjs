0f3a4ae4086bdc1a402e6bc36bad2bcc9039a596
Hayden <haydenk@users.noreply.github.com>
2024-02-14 16:40:50 -0600

    add basePath to nextConfig when exporting

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const environment = process.env.NODE_ENV ||= "development";
 /** @type {import('next').NextConfig} */
 let nextConfig = {};
 if (environment === "production") {
-    nextConfig = {...nextConfig, output: "export", images: {unoptimized: true}}
+    nextConfig = {...nextConfig, output: "export", images: {unoptimized: true}, basePath: "waffle-fries-dev/"}
 }
 
 export default nextConfig;
